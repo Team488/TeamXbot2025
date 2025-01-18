a51b4946efcb8173d015b19e0e05907068b3addc
Alex Schokking <aschokking@gmail.com>
2025-01-18 14:43:24 -0800

    remove defunct import

--- src/main/java/competition/electrical_contract/ElectricalContract.java
+++ src/main/java/competition/electrical_contract/ElectricalContract.java
@@ -1,9 +1,7 @@
 package competition.electrical_contract;
 
 import xbot.common.injection.electrical_contract.CANMotorControllerInfo;
-import xbot.common.injection.electrical_contract.CANTalonInfo;
 import xbot.common.injection.electrical_contract.DeviceInfo;
-import xbot.common.injection.electrical_contract.MotorControllerType;
 import xbot.common.injection.electrical_contract.XCameraElectricalContract;
 import xbot.common.injection.electrical_contract.XSwerveDriveElectricalContract;
 import xbot.common.injection.swerve.SwerveInstance;
