10b2e68b078a27198202d6d54a0f2b69a6f043f5
Alex Schokking <aschokking@gmail.com>
2025-01-20 22:09:37 -0800

    Create a demo command to set the elevator properties, this shouldn't be committed

--- src/main/java/competition/subsystems/SubsystemDefaultCommandMap.java
+++ src/main/java/competition/subsystems/SubsystemDefaultCommandMap.java
@@ -7,6 +7,8 @@ import competition.subsystems.algae_collection.AlgaeCollectionSubsystem;
 import competition.subsystems.algae_collection.commands.AlgaeCollectionStopCommand;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.drive.commands.SwerveDriveWithJoysticksCommand;
+import competition.subsystems.elevator.ElevatorMechanism;
+import competition.subsystems.elevator.ElevatorMechanismDemoCommand;
 
 /**
  * For setting the default commands on subsystems
@@ -19,11 +21,17 @@ public class SubsystemDefaultCommandMap {
 
     @Inject
     public void setupDriveSubsystem(DriveSubsystem driveSubsystem, SwerveDriveWithJoysticksCommand command) {
-        driveSubsystem.setDefaultCommand(command);
+        // temp disable drive to use gamepad for elevator demo
+        //driveSubsystem.setDefaultCommand(command);
     }
 
     @Inject
     public void setupAlgaeCollectionSubsystem(AlgaeCollectionSubsystem algaeCollectionSubsystem, AlgaeCollectionStopCommand command) {
         algaeCollectionSubsystem.setDefaultCommand(command);
+    }
+
+    @Inject
+    public void setupElevatorMechanism(ElevatorMechanism elevatorMechanism, ElevatorMechanismDemoCommand command) {
+        elevatorMechanism.setDefaultCommand(command);
     }
 }
