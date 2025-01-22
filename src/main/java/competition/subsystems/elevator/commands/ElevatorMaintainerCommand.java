0be5b1dc44c5775f632eb7db56c04c3439963642
Junyu Zhu <120147135+junyu101@users.noreply.github.com>
2025-01-21 20:52:57 -0800

    Updated ElevatorSubsystem Skeleton Setup (#24)
    * Basic Elevator Skeleton Setup Done
    
    * Basic Elevator Skeleton Setup
    
    * Small updates to naming
    
    * Delete unneeded imports
    
    * Deleting MORE unneeded imports...
    
    * Refactor Code to BaseSetpointSubsystem
    
    * Add Overrides
    
    * End of Saturday: Conversions of Classes from Double to Distance in Progess
    
    * Refactoring from Double to Distance for ElevatorSubsystem and maintainer
    
    * Update SeriouslyCommonLib
    
    * Deleted Unneeded Commands, added setTargetHeight for goalHeight enum
    
    * stupid checkstyle

---
+++ src/main/java/competition/subsystems/elevator/commands/ElevatorMaintainerCommand.java
@@ -0,0 +1,71 @@
+package competition.subsystems.elevator.commands;
+
+import competition.operator_interface.OperatorInterface;
+import competition.subsystems.elevator.ElevatorSubsystem;
+import edu.wpi.first.units.measure.Distance;
+import xbot.common.command.BaseCommand;
+import xbot.common.command.BaseMaintainerCommand;
+import xbot.common.command.BaseSubsystem;
+import xbot.common.logic.HumanVsMachineDecider;
+import xbot.common.math.PIDManager;
+import xbot.common.properties.PropertyFactory;
+
+import javax.inject.Inject;
+
+public class ElevatorMaintainerCommand extends BaseMaintainerCommand<Distance> {
+
+    public enum MaintainerMode{
+        Calibrating,
+        GaveUp,
+        Calibrated,
+    }
+
+    ElevatorSubsystem elevator;
+
+    @Inject
+    public ElevatorMaintainerCommand(ElevatorSubsystem elevator, PropertyFactory pf,
+                                     HumanVsMachineDecider.HumanVsMachineDeciderFactory hvmFactory,
+                                     PIDManager.PIDManagerFactory pidf,
+                                     OperatorInterface oi){
+        super(elevator,pf,hvmFactory, 1, 0.2);
+
+    }
+
+    @Override
+    public void initialize() {
+        log.info("initializing");
+
+    }
+
+    @Override
+    public void execute(){
+
+    }
+
+    @Override
+    protected void coastAction() {
+
+    }
+
+    @Override
+    protected void calibratedMachineControlAction() {
+
+    }
+
+    @Override
+    protected double getErrorMagnitude() {
+        return 0;
+    }
+
+    @Override
+    protected double getHumanInput() {
+
+        return 0;
+    }
+
+    @Override
+    protected double getHumanInputMagnitude() {
+        return 0;
+    }
+
+}
