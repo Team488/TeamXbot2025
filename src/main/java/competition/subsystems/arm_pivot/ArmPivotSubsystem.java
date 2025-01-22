3e3edbe2cff5a78648d8d83e2bc50bf39c1d6c13
Alan <alanyuan709@gmail.com>
2025-01-21 20:44:21 -0800

    Update ArmPivotSubsystem.java
    set point system

--- src/main/java/competition/subsystems/arm_pivot/ArmPivotSubsystem.java
+++ src/main/java/competition/subsystems/arm_pivot/ArmPivotSubsystem.java
@@ -1,6 +1,7 @@
 package competition.subsystems.arm_pivot;
 
 import competition.electrical_contract.ElectricalContract;
+import xbot.common.command.BaseSetpointSubsystem;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANMotorController;
 import xbot.common.properties.PropertyFactory;
@@ -9,7 +10,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
-public class ArmPivotSubsystem extends BaseSubsystem {
+public class ArmPivotSubsystem extends BaseSetpointSubsystem {
     public final XCANMotorController armMotor;
 
     @Inject
@@ -25,4 +26,33 @@ public class ArmPivotSubsystem extends BaseSubsystem {
         }
     }
 
+    @Override
+    public Object getCurrentValue() {
+        return null;
+    }
+
+    @Override
+    public Object getTargetValue() {
+        return null;
+    }
+
+    @Override
+    public void setTargetValue(Object value) {
+
+    }
+
+    @Override
+    public void setPower(Object power) {
+
+    }
+
+    @Override
+    public boolean isCalibrated() {
+        return false;
+    }
+
+    @Override
+    protected boolean areTwoTargetsEquivalent(Object target1, Object target2) {
+        return false;
+    }
 }
