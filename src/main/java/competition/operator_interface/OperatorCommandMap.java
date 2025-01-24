b4c0f247dc52b09f5a02fff09976826be44a3a3d
Rong <ronrzhu@gmail.com>
2025-01-23 20:49:29 -0800

    End of day 1/23

--- src/main/java/competition/operator_interface/OperatorCommandMap.java
+++ src/main/java/competition/operator_interface/OperatorCommandMap.java
@@ -1,14 +1,22 @@
 package competition.operator_interface;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 
 import competition.simulation.commands.ResetSimulatedPose;
 import competition.subsystems.drive.commands.DebugSwerveModuleCommand;
 import competition.subsystems.drive.commands.SwerveDriveWithJoysticksCommand;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import xbot.common.controls.sensors.XXboxController;
+import xbot.common.subsystems.drive.SwerveSimpleTrajectoryCommand;
 import xbot.common.subsystems.drive.swerve.commands.ChangeActiveSwerveModuleCommand;
 import xbot.common.subsystems.pose.commands.SetRobotHeadingCommand;
+import xbot.common.trajectory.XbotSwervePoint;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Maps operator interface buttons to commands
@@ -23,9 +31,29 @@ public class OperatorCommandMap {
     @Inject
     public void setupMyCommands(
             OperatorInterface operatorInterface,
-            SetRobotHeadingCommand resetHeading) {
+            SetRobotHeadingCommand resetHeading,
+            Provider<SwerveSimpleTrajectoryCommand> provider) {
         resetHeading.setHeadingToApply(0);
-        operatorInterface.gamepad.getifAvailable(1).onTrue(resetHeading);
+        //operatorInterface.gamepad.getifAvailable(1).onTrue(resetHeading);
+
+        var toOrigin = provider.get();
+        List<XbotSwervePoint> toOriginPoints = new ArrayList<>();
+        toOriginPoints.add(new XbotSwervePoint(new Translation2d(6,2), new Rotation2d(0), 10));
+        toOriginPoints.add(new XbotSwervePoint(new Translation2d(2,2), new Rotation2d(0), 10));
+        toOrigin.logic.setEnableConstantVelocity(true);
+        toOrigin.logic.setConstantVelocity(2);
+        toOrigin.logic.setKeyPoints(toOriginPoints);
+
+        var toGoal = provider.get();
+        List<XbotSwervePoint> toGoalPoints = new ArrayList<>();
+        toGoalPoints.add(new XbotSwervePoint(new Translation2d(14,2), new Rotation2d(0), 10));
+        toGoalPoints.add(new XbotSwervePoint(new Translation2d(2,2), new Rotation2d(0), 10));
+        toGoal.logic.setEnableConstantVelocity(true);
+        toGoal.logic.setConstantVelocity(2);
+        toGoal.logic.setKeyPoints(toGoalPoints);
+
+        operatorInterface.gamepad.getifAvailable(XXboxController.XboxButton.B).onTrue(toOrigin);
+        operatorInterface.gamepad.getifAvailable(XXboxController.XboxButton.X).onTrue(toGoal);
     }
 
     // Programmer commands are only meant to be used to debug or test the robot. They should not be used in competition,
@@ -37,9 +65,9 @@ public class OperatorCommandMap {
             DebugSwerveModuleCommand debugModule,
             ChangeActiveSwerveModuleCommand changeActiveModule,
             SwerveDriveWithJoysticksCommand typicalSwerveDrive) {
-        oi.programmerGamepad.getifAvailable(XXboxController.XboxButton.X).onTrue(changeActiveModule);
-        oi.programmerGamepad.getifAvailable(XXboxController.XboxButton.A).onTrue(debugModule);
-        oi.programmerGamepad.getifAvailable(XXboxController.XboxButton.B).onTrue(typicalSwerveDrive);
+//        oi.programmerGamepad.getifAvailable(XXboxController.XboxButton.X).onTrue(changeActiveModule);
+//        oi.programmerGamepad.getifAvailable(XXboxController.XboxButton.A).onTrue(debugModule);
+//        oi.programmerGamepad.getifAvailable(XXboxController.XboxButton.B).onTrue(typicalSwerveDrive);
     }
 
     @Inject
