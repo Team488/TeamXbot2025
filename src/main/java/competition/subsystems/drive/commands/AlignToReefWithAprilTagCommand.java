fc786389442202a217d12b3225dc44be585cfce9
kujo27 <johnkuang783@gmail.com>
2025-01-25 15:23:31 -0800

    using default move method for now

--- src/main/java/competition/subsystems/drive/commands/AlignToReefWithAprilTagCommand.java
+++ src/main/java/competition/subsystems/drive/commands/AlignToReefWithAprilTagCommand.java
@@ -49,7 +49,7 @@ public class AlignToReefWithAprilTagCommand extends BaseCommand {
 
         double omega = headingModule.calculateHeadingPower(getClosestReefFacePose().getRotation().getDegrees());
 
-        drive.move(new XYPair(-driveValues.getX(), -driveValues.getY()), omega, pose.getCurrentPose2d());
+        drive.move(new XYPair(-driveValues.getX(), -driveValues.getY()), omega);
 
         aKitLog.record("dx power", driveValues.getX());
         aKitLog.record("dy power", driveValues.getY());
