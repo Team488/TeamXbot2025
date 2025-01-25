5347adaa6bf10c361bfab3ededf1054ab09c4a1f
Junyu Zhu <120147135+junyu101@users.noreply.github.com>
2025-01-25 14:13:02 -0800

    Fixing merge conflicts

--- src/main/java/competition/electrical_contract/UnitTestContract2025.java
+++ src/main/java/competition/electrical_contract/UnitTestContract2025.java
@@ -6,24 +6,4 @@ public class UnitTestContract2025 extends Contract2025 {
 
     @Inject
     public UnitTestContract2025() {}
-
-    @Override
-    public boolean isDriveReady(){
-        return true;
-    }
-
-    @Override
-    public boolean isAlgaeCollectionReady(){
-        return true;
-    }
-
-    @Override
-    public boolean isCoralCollectionMotorReady(){
-        return true;
-    }
-
-    @Override
-    public boolean isElevatorReady(){
-        return true;
-    }
 }
