fbd84acc121014600b31900fef0d126f52d250bb
Junyu Zhu <120147135+junyu101@users.noreply.github.com>
2025-01-25 13:59:51 -0800

    Reapply "Fixing merge conflicts with arm pivot subsystem"
    This reverts commit 688dbb2379cb9bf25641e0d5e8b185322d9c3074.

--- src/main/java/competition/Robot.java
+++ src/main/java/competition/Robot.java
@@ -38,6 +38,7 @@ public class Robot extends BaseRobot {
         dataFrameRefreshables.add(getInjectorComponent().visionSubsystem());
         dataFrameRefreshables.add(getInjectorComponent().aprilTagVisionSubsystem());
         dataFrameRefreshables.add(getInjectorComponent().elevatorSubsystem());
+        dataFrameRefreshables.add(getInjectorComponent().armPivotSubsystem());
     }
 
     protected BaseRobotComponent createDaggerComponent() {
