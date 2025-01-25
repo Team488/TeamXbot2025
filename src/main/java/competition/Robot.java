a831ad92986885bf05f4d8f720485e7c4b13702c
AlanYuan99 <alanyuan709@gmail.com>
2025-01-25 15:17:41 -0800

    Coral sensor (#41)
    * Unit Test
    
    made booleans true
    
    * added more booleans
    
    * Created code for coral sensor
    
    * cleaned up code and fixed hasCoral method
    
    * Added aKitLogger for coral and removed isCoralReady boolean
    
    * updated unit test
    
    * use mains SCL
    
    ---------
    
    Co-authored-by: Alex Schokking <aschokking@gmail.com>

--- src/main/java/competition/Robot.java
+++ src/main/java/competition/Robot.java
@@ -37,6 +37,7 @@ public class Robot extends BaseRobot {
         dataFrameRefreshables.add(getInjectorComponent().visionSubsystem());
         dataFrameRefreshables.add(getInjectorComponent().aprilTagVisionSubsystem());
         dataFrameRefreshables.add(getInjectorComponent().armPivotSubsystem());
+        dataFrameRefreshables.add(getInjectorComponent().coralScorerSubsystem());
     }
 
     protected BaseRobotComponent createDaggerComponent() {
--- src/main/java/competition/injection/components/BaseRobotComponent.java
+++ src/main/java/competition/injection/components/BaseRobotComponent.java
@@ -4,6 +4,7 @@ import competition.operator_interface.OperatorCommandMap;
 import competition.simulation.BaseSimulator;
 import competition.subsystems.SubsystemDefaultCommandMap;
 import competition.subsystems.arm_pivot.ArmPivotSubsystem;
+import competition.subsystems.coral_scorer.CoralScorerSubsystem;
 import competition.subsystems.elevator.ElevatorMechanism;
 import competition.subsystems.vision.VisionSubsystem;
 import xbot.common.injection.components.BaseComponent;
@@ -29,5 +30,7 @@ public abstract class BaseRobotComponent extends BaseComponent {
     public abstract ElevatorMechanism elevatorMechanism();
 
     public abstract ArmPivotSubsystem armPivotSubsystem();
+
+    public abstract CoralScorerSubsystem coralScorerSubsystem();
 
 }
