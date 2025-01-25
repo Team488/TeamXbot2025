9e64359c9ce12c77e6b131f32c6d0eea183c34b7
Alan <alanyuan709@gmail.com>
2025-01-25 13:41:28 -0800

    Created code for coral sensor

--- src/main/java/competition/subsystems/coral_scorer/CoralScorerSubsystem.java
+++ src/main/java/competition/subsystems/coral_scorer/CoralScorerSubsystem.java
@@ -3,7 +3,8 @@ package competition.subsystems.coral_scorer;
 import competition.electrical_contract.ElectricalContract;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANMotorController;
-import xbot.common.controls.actuators.XCANMotorControllerPIDProperties;
+import xbot.common.controls.sensors.XDigitalInput;
+import xbot.common.properties.BooleanProperty;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
@@ -15,10 +16,13 @@ public class CoralScorerSubsystem extends BaseSubsystem {
     public XCANMotorController motor = null;
     public DoubleProperty intakePower;
     public DoubleProperty scorePower;
+    public XDigitalInput coralSensor;
+    public BooleanProperty isCoralReady;
 
     @Inject
     public CoralScorerSubsystem(XCANMotorController.XCANMotorControllerFactory xcanMotorControllerFactory,
-                                ElectricalContract electricalContract, PropertyFactory propertyFactory) {
+                                ElectricalContract electricalContract, PropertyFactory propertyFactory,
+                                XDigitalInput.XDigitalInputFactory xDigitalInputFactory) {
         propertyFactory.setPrefix(this);
         if (electricalContract.isCoralCollectionMotorReady()) {
             this.motor = xcanMotorControllerFactory.create(electricalContract.getCoralCollectionMotor(),
@@ -30,8 +34,17 @@ public class CoralScorerSubsystem extends BaseSubsystem {
             this.motor = null;
         }
 
+        if (electricalContract.isCoralSensorReady()) {
+            this.coralSensor = xDigitalInputFactory.create(electricalContract.getCoralSensor(),
+                    "CoralSensor");
+        } else {
+            this.coralSensor = null;
+        }
+
         this.intakePower = propertyFactory.createPersistentProperty("intakePower", .1);
         this.scorePower = propertyFactory.createPersistentProperty("scorerPower", -.1);
+
+        this.isCoralReady = propertyFactory.createPersistentProperty("isCoralReady", false);
     }
 
     public void intake() {
@@ -39,17 +52,21 @@ public class CoralScorerSubsystem extends BaseSubsystem {
             motor.setPower(intakePower.get());
         }
     }
-    public void scorer(){
+    public void scorer() {
         if (motor != null){
             motor.setPower(scorePower.get());
         }
     }
-    public void stop(){
+    public void stop() {
         if (motor!=null){
             motor.setPower(0);
         }
     }
-
-
+    public boolean hasCoral() {
+        if (coralSensor != null) {
+            isCoralReady.set(true);
+        }
+        return isCoralReady.get();
+    }
 }
 
