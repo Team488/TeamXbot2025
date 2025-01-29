ca07daa44a04734fabd7aa4b0f11a5bff1f1ed19
Alex Schokking <aschokking@gmail.com>
2025-01-28 18:06:44 -0800

    fix bad merge

--- src/main/java/competition/simulation/MapleSimulator.java
+++ src/main/java/competition/simulation/MapleSimulator.java
@@ -42,6 +42,8 @@ public class MapleSimulator implements BaseSimulator {
     final Distance humanLoadingDistanceThreshold = Meters.of(0.5);
 
     // maple-sim stuff ----------------------------
+    final DriveTrainSimulationConfig config;
+    final SimulatedArena arena;
     final SelfControlledSwerveDriveSimulation swerveDriveSimulation;
 
     @Inject
@@ -51,6 +53,8 @@ public class MapleSimulator implements BaseSimulator {
         this.drive = drive;
         this.elevatorSimulator = elevatorSimulator;
         this.armSimulator = armSimulator;
+        this.reefSimulator = reefSimulator;
+        this.coralScorerSimulator = coralScorerSimulator;
 
         aKitLog = new AKitLogger("Simulator/");
 
