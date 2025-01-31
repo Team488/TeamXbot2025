7460fc87954c8340640691539e3f63ab035e24e5
kujo27 <johnkuang783@gmail.com>
2025-01-30 19:32:33 -0800

    finished

---
+++ src/main/java/competition/subsystems/coral_scorer/commands/ScoreWhenReadyCommand.java
@@ -0,0 +1,41 @@
+package competition.subsystems.coral_scorer.commands;
+
+import competition.subsystems.arm_pivot.ArmPivotSubsystem;
+import competition.subsystems.coral_scorer.CoralScorerSubsystem;
+import competition.subsystems.elevator.ElevatorSubsystem;
+import xbot.common.command.BaseCommand;
+
+import javax.inject.Inject;
+
+public class ScoreWhenReadyCommand extends BaseCommand {
+    CoralScorerSubsystem coralScorerSubsystem;
+    ArmPivotSubsystem armPivotSubsystem;
+    ElevatorSubsystem elevatorSubsystem;
+
+    @Inject
+    public ScoreWhenReadyCommand(CoralScorerSubsystem coralScorerSubsystem, ArmPivotSubsystem armPivotSubsystem,
+                                 ElevatorSubsystem elevatorSubsystem) {
+        this.coralScorerSubsystem = coralScorerSubsystem;
+        this.armPivotSubsystem = armPivotSubsystem;
+        this.elevatorSubsystem = elevatorSubsystem;
+        addRequirements(coralScorerSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        log.info("Initializing");
+    }
+
+    @Override
+    public void execute() {
+        if (coralScorerSubsystem.hasCoral() && armPivotSubsystem.isMaintainerAtGoal()
+                && elevatorSubsystem.isMaintainerAtGoal()) {
+            coralScorerSubsystem.scorer();
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return coralScorerSubsystem.confidentlyHasScoredCoral();
+    }
+}
