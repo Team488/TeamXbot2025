48891504319f376b7489bf757577f5fe4517eb12
Alex Schokking <aschokking@gmail.com>
2025-02-01 13:06:35 -0800

    Clone coral arm sim to make the algae arm sim base

---
+++ src/main/java/competition/simulation/algae_arm/AlgaeArmSimConstants.java
@@ -0,0 +1,22 @@
+package competition.simulation.algae_arm;
+
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Kilogram;
+import static edu.wpi.first.units.Units.Meters;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Mass;
+
+public class AlgaeArmSimConstants {
+    // TODO: this is all from the wpilib example but doesn't reflect our real math
+    public static final double armReduction = 200;
+    public static final Mass armMass = Kilogram.of(8.0); // Kilograms
+    public static final Distance armLength = Meters.of(0.5);
+    // the frame of reference for these angles is 0' right, 90' up, 180' left, 270' down
+    // so we have 225 as the starting angle which is 0' in arm relative terms
+    public static final Angle minAngleRads = Degrees.of(225 - 125);
+    public static final Angle maxAngleRads = Degrees.of(225);
+    public static final Angle startingAngle = maxAngleRads;
+    public static final Angle armEncoderAnglePerRotation = Degrees.of(0.1);
+}
--- src/main/java/competition/simulation/algae_arm/AlgaeArmSimulator.java
+++ src/main/java/competition/simulation/algae_arm/AlgaeArmSimulator.java
@@ -1,10 +1,10 @@
-package competition.simulation.coral_arm;
+package competition.simulation.algae_arm;
 
 import javax.inject.Inject;
 
 import competition.simulation.MotorInternalPIDHelper;
 import competition.simulation.SimulationConstants;
-import competition.subsystems.coral_arm_pivot.CoralArmPivotSubsystem;
+import competition.subsystems.algae_arm.AlgaeArmSubsystem;
 
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Radians;
@@ -19,16 +19,16 @@ import xbot.common.controls.actuators.mock_adapters.MockCANMotorController;
 import xbot.common.math.PIDManager;
 import xbot.common.properties.PropertyFactory;
 
-public class CoralArmSimulator {
+public class AlgaeArmSimulator {
     final DCMotor motor = DCMotor.getKrakenX60(1);
     final SingleJointedArmSim armSim;
     final PIDManager pidManager;
 
-    final CoralArmPivotSubsystem armPivotSubsystem;
+    final AlgaeArmSubsystem armPivotSubsystem;
     final MockCANMotorController armMotor;
 
     @Inject
-    public CoralArmSimulator(CoralArmPivotSubsystem armPivotSubsystem, PIDManager.PIDManagerFactory pidManagerFactory, PropertyFactory pf) {
+    public AlgaeArmSimulator(AlgaeArmSubsystem armPivotSubsystem, PIDManager.PIDManagerFactory pidManagerFactory, PropertyFactory pf) {
         pf.setPrefix("CoralArmSimulator");
         this.pidManager = pidManagerFactory.create(pf.getPrefix() + "/CANMotorPositionalPID", 0.01, 0.001, 0.0, 0.0, 1.0, -1.0);
         this.armPivotSubsystem = armPivotSubsystem;
@@ -36,14 +36,14 @@ public class CoralArmSimulator {
 
         this.armSim = new SingleJointedArmSim(
                 motor,
-                CoralArmSimConstants.armReduction,
-                SingleJointedArmSim.estimateMOI(CoralArmSimConstants.armLength.in(Meters),
-                        CoralArmSimConstants.armMass.in(Kilograms)),
-                CoralArmSimConstants.armLength.in(Meters),
-                CoralArmSimConstants.minAngleRads.in(Radians),
-                CoralArmSimConstants.maxAngleRads.in(Radians),
+                AlgaeArmSimConstants.armReduction,
+                SingleJointedArmSim.estimateMOI(AlgaeArmSimConstants.armLength.in(Meters),
+                        AlgaeArmSimConstants.armMass.in(Kilograms)),
+                AlgaeArmSimConstants.armLength.in(Meters),
+                AlgaeArmSimConstants.minAngleRads.in(Radians),
+                AlgaeArmSimConstants.maxAngleRads.in(Radians),
                 true,
-                CoralArmSimConstants.startingAngle.in(Radians));
+                AlgaeArmSimConstants.startingAngle.in(Radians));
     }
 
     public void update() {
@@ -57,7 +57,7 @@ public class CoralArmSimulator {
         // Read out the new arm position for rendering
         var armRelativeAngle = getArmAngle();
 
-        var armMotorRotations = armRelativeAngle.in(Radians) / CoralArmSimConstants.armEncoderAnglePerRotation.in(Radians);
+        var armMotorRotations = armRelativeAngle.in(Radians) / AlgaeArmSimConstants.armEncoderAnglePerRotation.in(Radians);
         armMotor.setPosition(Rotations.of(armMotorRotations));
     }
 
@@ -66,7 +66,7 @@ public class CoralArmSimulator {
         // reference where the bottom is 0' and the top is 125'
         var armSimAngle = Radians.of(armSim.getAngleRads());
 
-        return armSimAngle.minus(CoralArmSimConstants.maxAngleRads).times(-1);
+        return armSimAngle.minus(AlgaeArmSimConstants.maxAngleRads).times(-1);
     }
 
     public boolean isAtCollectionAngle() {
