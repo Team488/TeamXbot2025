7fcad8166909ee88ab486f574f2ed2783c2905db
anthonytrenh <trenhanthony@gmail.com>
2025-02-01 11:37:20 -0800

    name change because commandgroup uses both elevator and arm

--- src/main/java/competition/commandgroups/PrepCoralSystemCommandGroupFactory.java
+++ src/main/java/competition/commandgroups/PrepCoralSystemCommandGroupFactory.java
@@ -6,13 +6,13 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import javax.inject.Inject;
 
-public class PrepCoralArmCommandGroupFactory {
+public class PrepCoralSystemCommandGroupFactory {
 
     SetElevatorTargetHeightCommand setElevatorTargetHeightCommand;
 
 
     @Inject
-    public PrepCoralArmCommandGroupFactory(SetElevatorTargetHeightCommand setElevatorTargetHeightCommand
+    public PrepCoralSystemCommandGroupFactory(SetElevatorTargetHeightCommand setElevatorTargetHeightCommand
                                         ) {
         this.setElevatorTargetHeightCommand = setElevatorTargetHeightCommand;
     }
