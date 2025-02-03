aa1e3bf862f6edb70025be2bc8a153737abdd573
anthonytrenh <trenhanthony@gmail.com>
2025-02-03 04:15:02 -0800

    forgot to change the reef face for EnumsToPose hashmap values

--- src/main/java/competition/subsystems/pose/EnumsToPose.java
+++ src/main/java/competition/subsystems/pose/EnumsToPose.java
@@ -10,16 +10,16 @@ public class EnumsToPose {
     public EnumsToPose() {
         addToMap(Landmarks.ReefFace.CLOSE, Landmarks.Branch.A, Landmarks.BlueCloseBranchA);
         addToMap(Landmarks.ReefFace.CLOSE, Landmarks.Branch.B, Landmarks.BlueCloseBranchB);
-        addToMap(Landmarks.ReefFace.CLOSE_LEFT, Landmarks.Branch.A, Landmarks.BlueCloseBranchA);
-        addToMap(Landmarks.ReefFace.CLOSE_LEFT, Landmarks.Branch.B, Landmarks.BlueCloseBranchB);
-        addToMap(Landmarks.ReefFace.CLOSE_RIGHT, Landmarks.Branch.A, Landmarks.BlueCloseBranchA);
-        addToMap(Landmarks.ReefFace.CLOSE_RIGHT, Landmarks.Branch.B, Landmarks.BlueCloseBranchB);
-        addToMap(Landmarks.ReefFace.FAR, Landmarks.Branch.A, Landmarks.BlueCloseBranchA);
-        addToMap(Landmarks.ReefFace.FAR, Landmarks.Branch.B, Landmarks.BlueCloseBranchB);
-        addToMap(Landmarks.ReefFace.FAR_LEFT, Landmarks.Branch.A, Landmarks.BlueCloseBranchA);
-        addToMap(Landmarks.ReefFace.FAR_LEFT, Landmarks.Branch.B, Landmarks.BlueCloseBranchB);
-        addToMap(Landmarks.ReefFace.FAR_RIGHT, Landmarks.Branch.A, Landmarks.BlueCloseBranchA);
-        addToMap(Landmarks.ReefFace.FAR_RIGHT, Landmarks.Branch.B, Landmarks.BlueCloseBranchB);
+        addToMap(Landmarks.ReefFace.CLOSE_LEFT, Landmarks.Branch.A, Landmarks.BlueCloseLeftBranchA);
+        addToMap(Landmarks.ReefFace.CLOSE_LEFT, Landmarks.Branch.B, Landmarks.BlueCloseLeftBranchB);
+        addToMap(Landmarks.ReefFace.CLOSE_RIGHT, Landmarks.Branch.A, Landmarks.BlueCloseRightBranchA);
+        addToMap(Landmarks.ReefFace.CLOSE_RIGHT, Landmarks.Branch.B, Landmarks.BlueCloseRightBranchB);
+        addToMap(Landmarks.ReefFace.FAR, Landmarks.Branch.A, Landmarks.BlueFarBranchA);
+        addToMap(Landmarks.ReefFace.FAR, Landmarks.Branch.B, Landmarks.BlueFarBranchB);
+        addToMap(Landmarks.ReefFace.FAR_LEFT, Landmarks.Branch.A, Landmarks.BlueFarLeftBranchA);
+        addToMap(Landmarks.ReefFace.FAR_LEFT, Landmarks.Branch.B, Landmarks.BlueFarLeftBranchB);
+        addToMap(Landmarks.ReefFace.FAR_RIGHT, Landmarks.Branch.A, Landmarks.BlueFarRightBranchA);
+        addToMap(Landmarks.ReefFace.FAR_RIGHT, Landmarks.Branch.B, Landmarks.BlueFarRightBranchB);
 
         addToMap(Landmarks.ReefFace.CLOSE, Landmarks.BlueCloseAlgae);
         addToMap(Landmarks.ReefFace.CLOSE_LEFT, Landmarks.BlueCloseLeftAlgae);
