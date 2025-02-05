179ce032348610c8311e1bb185e06a6c2d5302e1
Stephen Just <stephenjust@gmail.com>
2025-02-04 19:54:41 -0800

    Placate the robots

--- src/test/java/competition/motion/TrapezoidProfileManagerTest.java
+++ src/test/java/competition/motion/TrapezoidProfileManagerTest.java
@@ -3,7 +3,7 @@ package competition.motion;
 import competition.BaseCompetitionTest;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class TrapezoidProfileManagerTest extends BaseCompetitionTest {
 
