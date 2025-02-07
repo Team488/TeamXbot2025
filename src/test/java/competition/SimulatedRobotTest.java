edf149585d6dcc36e0b0e032413179249b641f99
Stephen Just <stephenjust@gmail.com>
2025-02-06 19:50:50 -0800

    Fix test since BaseRobot::start calls System.exit internally, we need to bypass it

--- src/test/java/competition/SimulatedRobotTest.java
+++ src/test/java/competition/SimulatedRobotTest.java
@@ -1,9 +1,8 @@
 package competition;
 
-import competition.electrical_contract.ElectricalContract;
 import competition.electrical_contract.UnitTestContract2025;
+import edu.wpi.first.wpilibj.RobotBase;
 import org.junit.Test;
-import xbot.common.command.BaseRobot;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,24 +12,29 @@ import static org.junit.Assert.fail;
 public class SimulatedRobotTest {
     @Test
     public void testSimulatedRobot() {
+        RobotBase.suppressExitWarning(true);
         try (var robot = new Robot()) {
             robot.simulatorContract = new UnitTestContract2025();
 
-            Thread robotThread = new Thread(() -> {
-                BaseRobot.startRobot(() -> robot);
-            });
+            Thread robotThread = new Thread(robot::startCompetition);
             robotThread.start();
 
             try {
-                var passDisabledInit = robot.reachedDisabledInit.tryAcquire(10, TimeUnit.SECONDS);
+                var passDisabledInit = robot.reachedDisabledInit.tryAcquire(5, TimeUnit.SECONDS);
                 assertTrue("Robot did not reach disabled init", passDisabledInit);
 
-                var passDisabledPeriodic = robot.reachedDisabledPeriodic.tryAcquire(10, TimeUnit.SECONDS);
+                var passDisabledPeriodic = robot.reachedDisabledPeriodic.tryAcquire(5, TimeUnit.SECONDS);
                 assertTrue("Robot did not reach disabled periodic", passDisabledPeriodic);
             } catch (InterruptedException e) {
                 fail("Robot test was interrupted");
             } finally {
                 robot.endCompetition();
+
+                try {
+                    robotThread.join(1000);
+                } catch (InterruptedException e) {
+                    fail("Thread was interrupted");
+                }
             }
         }
     }
