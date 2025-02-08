b48fe4c407930128a25399275c525d8cfdc24e27
Alex Schokking <aschokking@gmail.com>
2025-02-07 21:05:11 -0800

    add a little animation

--- src/main/java/competition/simulation/LightsSimulator.java
+++ src/main/java/competition/simulation/LightsSimulator.java
@@ -7,6 +7,7 @@ import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
 
 import competition.subsystems.lights.LightSubsystem;
 import competition.subsystems.lights.LightSubsystem.LightsStateMessage;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import xbot.common.advantage.AKitLogger;
@@ -18,18 +19,23 @@ public class LightsSimulator {
     final LoggedMechanism2d lightsMechanism;
     final LoggedMechanismLigament2d lightsLigament;
 
+    int frame;
+
     @Inject
     public LightsSimulator(LightSubsystem lightSubsystem) {
         this.lightSubsystem = lightSubsystem;
         this.aKitLog = new AKitLogger("Simulator/");
 
         this.lightsMechanism = new LoggedMechanism2d(1, 1);
-        var root = this.lightsMechanism.getRoot("lightsRoot", 0.55, 0.2);
-        this.lightsLigament = new LoggedMechanismLigament2d("lights", 0.5, 90, 14, new Color8Bit(Color.kGreen));
+        var root = this.lightsMechanism.getRoot("lightsRoot", 0.7, 0.2);
+        this.lightsLigament = new LoggedMechanismLigament2d("lights", 0.5, 90, 14, new Color8Bit(Color.kBlack));
         root.append(lightsLigament);
     }
 
     Color8Bit getColorForSubsystemState(LightsStateMessage state) {
+        if(!DriverStation.isDSAttached()) {
+            return new Color8Bit(Color.kGray);
+        }
         switch (state) {
             case RobotDisabled:
                 return new Color8Bit(Color.kRed);
@@ -47,6 +53,8 @@ public class LightsSimulator {
     }
 
     public void update() {
+        this.frame++;
+        this.lightsLigament.setLineWeight(Math.cos(frame * Math.PI / 50) * 1 + 14);
         this.lightsLigament.setColor(getColorForSubsystemState(lightSubsystem.getState()));
         aKitLog.record("FieldSimulation/Lights", this.lightsMechanism);
 
