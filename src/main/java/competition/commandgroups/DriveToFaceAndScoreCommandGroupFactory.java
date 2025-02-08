ac0b06f1791368fe5110407d3c60b0659f73c18f
anthonytrenh <trenhanthony@gmail.com>
2025-02-08 14:33:18 -0800

    Update DriveToFaceAndScoreCommandGroupFactory.java

--- src/main/java/competition/commandgroups/DriveToFaceAndScoreCommandGroupFactory.java
+++ src/main/java/competition/commandgroups/DriveToFaceAndScoreCommandGroupFactory.java
@@ -1,11 +1,8 @@
 package competition.commandgroups;
 
-import competition.subsystems.coral_arm_pivot.CoralArmPivotSubsystem;
 import competition.subsystems.coral_scorer.commands.ScoreWhenReadyCommand;
-import competition.subsystems.elevator.ElevatorSubsystem;
 import competition.subsystems.pose.Landmarks;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import javax.inject.Inject;
