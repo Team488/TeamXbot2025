00a6bb8b339a93cd012e0f4bebd7c3c0b166f03e
Junyu Zhu <120147135+junyu101@users.noreply.github.com>
2025-02-08 13:37:12 -0800

    Cant calibrate without sensor

--- src/main/java/competition/subsystems/elevator/commands/ElevatorMaintainerCommand.java
+++ src/main/java/competition/subsystems/elevator/commands/ElevatorMaintainerCommand.java
@@ -1,5 +1,6 @@
 package competition.subsystems.elevator.commands;
 
+import competition.electrical_contract.ElectricalContract;
 import competition.motion.TrapezoidProfileManager;
 import competition.operator_interface.OperatorInterface;
 import competition.subsystems.elevator.ElevatorSubsystem;
@@ -40,19 +41,23 @@ public class ElevatorMaintainerCommand extends BaseMaintainerCommand<Distance> {
 
     final TrapezoidProfileManager profileManager;
 
+    final ElectricalContract contract;
+
     @Inject
     public ElevatorMaintainerCommand(ElevatorSubsystem elevator, PropertyFactory pf,
                                      HumanVsMachineDecider.HumanVsMachineDeciderFactory hvmFactory,
                                      CalibrationDecider.CalibrationDeciderFactory calibrationDeciderFactory,
                                      TrapezoidProfileManager.Factory trapezoidProfileManagerFactory,
                                      PIDManager.PIDManagerFactory pidf,
-                                     OperatorInterface oi){
+                                     OperatorInterface oi,
+                                     ElectricalContract contract){
         super(elevator, pf, hvmFactory, Inches.of(1).in(Meters), 0.2);
         pf.setPrefix(this);
         this.elevator = elevator;
         profileManager = trapezoidProfileManagerFactory.create(getPrefix() + "trapezoidMotion", 1, 1, elevator.getCurrentValue().in(Meters));
 
         this.oi = oi;
+        this.contract = contract;
 
         calibrationDecider = calibrationDeciderFactory.create("calibrationDecider");
         calibrationDecider.reset();
@@ -98,13 +103,16 @@ public class ElevatorMaintainerCommand extends BaseMaintainerCommand<Distance> {
 
         switch (mode){
             case Calibrated -> calibratedMachineControlAction();
-            case Attempting -> attemptCalibration();
+            case Attempting -> attemptCalibrationIfSensorExists();
             case GaveUp -> humanControlAction();
             default -> humanControlAction();
         }
     }
 
-    private void attemptCalibration(){
+    private void attemptCalibrationIfSensorExists(){
+        if(!contract.isElevatorBottomSensorReady()){
+            return;
+        }
         elevator.setPower(elevator.calibrationNegativePower.get());
 
         if (elevator.isTouchingBottom()){
