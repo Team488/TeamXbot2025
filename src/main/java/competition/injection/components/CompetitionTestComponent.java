7331edf3c0db45592a1811d15a15b286e027561a
Stephen Just <stephenjust@gmail.com>
2025-02-08 10:00:11 -0800

    Add a unit test with the drive marked as not ready

--- src/main/java/competition/injection/components/CompetitionTestComponent.java
+++ src/main/java/competition/injection/components/CompetitionTestComponent.java
@@ -2,9 +2,11 @@ package competition.injection.components;
 
 import javax.inject.Singleton;
 
+import competition.electrical_contract.ElectricalContract;
 import competition.injection.modules.CommonBinderModule;
 import competition.injection.modules.CommonProviderModule;
 import competition.injection.modules.UnitTestRobotModule;
+import dagger.BindsInstance;
 import dagger.Component;
 import xbot.common.injection.modules.MockControlsModule;
 import xbot.common.injection.modules.MockDevicesModule;
@@ -14,5 +16,11 @@ import xbot.common.injection.modules.UnitTestModule;
 @Component(modules = { UnitTestModule.class, MockDevicesModule.class, MockControlsModule.class,
         CommonProviderModule.class, CommonBinderModule.class, UnitTestRobotModule.class})
 public abstract class CompetitionTestComponent extends BaseRobotComponent {
-    
+    @Component.Builder
+    public abstract static class Builder {
+        @BindsInstance
+        public abstract CompetitionTestComponent.Builder electricalContract(ElectricalContract contract);
+
+        public abstract BaseRobotComponent build();
+    }
 }
