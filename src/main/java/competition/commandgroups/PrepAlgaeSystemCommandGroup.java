a4372420d99b59b27b347f7aa4ab66f77ebb80f0
kujo27 <johnkuang783@gmail.com>
2025-02-20 19:03:30 -0800

    created commandgroup

---
+++ src/main/java/competition/commandgroups/PrepAlgaeSystemCommandGroup.java
@@ -0,0 +1,37 @@
+package competition.commandgroups;
+
+import competition.subsystems.algae_arm.AlgaeArmSubsystem;
+import competition.subsystems.algae_arm.commands.SetAlgaeArmSetpointToTargetPosition;
+import competition.subsystems.algae_collection.commands.AlgaeCollectionIntakeCommand;
+import competition.subsystems.algae_collection.commands.AlgaeCollectionOutputCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+import javax.inject.Inject;
+
+public class PrepAlgaeSystemCommandGroup extends ParallelCommandGroup {
+    SetAlgaeArmSetpointToTargetPosition setAlgaeArmSetpointToTargetPosition;
+    AlgaeCollectionOutputCommand algaeCollectionOutputCommand;
+    AlgaeCollectionIntakeCommand algaeCollectionIntakeCommand;
+
+    @Inject
+    public PrepAlgaeSystemCommandGroup(SetAlgaeArmSetpointToTargetPosition setAlgaeArmSetpointToTargetPosition,
+                                       AlgaeCollectionOutputCommand algaeCollectionOutputCommand,
+                                       AlgaeCollectionIntakeCommand algaeCollectionIntakeCommand) {
+        this.setAlgaeArmSetpointToTargetPosition = setAlgaeArmSetpointToTargetPosition;
+        this.algaeCollectionOutputCommand = algaeCollectionOutputCommand;
+        this.algaeCollectionIntakeCommand = algaeCollectionIntakeCommand;
+    }
+
+    public ParallelCommandGroup create(AlgaeArmSubsystem.AlgaeArmPositions algaeArmPositions) {
+        var group  = new ParallelCommandGroup();
+
+        setAlgaeArmSetpointToTargetPosition.setTargetPosition(algaeArmPositions);
+        if (algaeArmPositions == AlgaeArmSubsystem.AlgaeArmPositions.GroundCollection) {
+            group.addCommands(setAlgaeArmSetpointToTargetPosition, algaeCollectionIntakeCommand);
+        }
+        else {
+            group.addCommands(setAlgaeArmSetpointToTargetPosition, algaeCollectionOutputCommand);
+        }
+        return group;
+    }
+}
