864d3109e4d1fd81c43958e0f1fc834f64427c0c
John Gilbert <johngilb@gmail.com>
2025-02-23 00:44:33 -0800

    Fix bug in red/blue vision system.

--- src/main/java/competition/subsystems/vision/AprilTagVisionSubsystemExtended.java
+++ src/main/java/competition/subsystems/vision/AprilTagVisionSubsystemExtended.java
@@ -32,12 +32,12 @@ public class AprilTagVisionSubsystemExtended extends AprilTagVisionSubsystem {
 
         // Note: flipped april tag IDs across the y-midpoint of the field for blue alliance
         // map both blue and red alliance poses
-        aprilTagIDHashMap.put(PoseSubsystem.convertBluetoRed(Landmarks.BlueCloseLeftAlgae), 8);
+        aprilTagIDHashMap.put(PoseSubsystem.convertBluetoRed(Landmarks.BlueCloseLeftAlgae), 6);
         aprilTagIDHashMap.put(PoseSubsystem.convertBluetoRed(Landmarks.BlueCloseAlgae), 7);
-        aprilTagIDHashMap.put(PoseSubsystem.convertBluetoRed(Landmarks.BlueCloseRightAlgae), 6);
-        aprilTagIDHashMap.put(PoseSubsystem.convertBluetoRed(Landmarks.BlueFarLeftAlgae), 9);
+        aprilTagIDHashMap.put(PoseSubsystem.convertBluetoRed(Landmarks.BlueCloseRightAlgae), 8);
+        aprilTagIDHashMap.put(PoseSubsystem.convertBluetoRed(Landmarks.BlueFarLeftAlgae), 11);
         aprilTagIDHashMap.put(PoseSubsystem.convertBluetoRed(Landmarks.BlueFarAlgae), 10);
-        aprilTagIDHashMap.put(PoseSubsystem.convertBluetoRed(Landmarks.BlueFarRightAlgae), 11);
+        aprilTagIDHashMap.put(PoseSubsystem.convertBluetoRed(Landmarks.BlueFarRightAlgae), 9);
         aprilTagIDHashMap.put(Landmarks.BlueCloseLeftAlgae, 19);
         aprilTagIDHashMap.put(Landmarks.BlueCloseAlgae, 18);
         aprilTagIDHashMap.put(Landmarks.BlueCloseRightAlgae, 17);
