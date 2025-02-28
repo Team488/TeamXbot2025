dfc1237f52ab3809e32b7cba69ebd02b6d92f894
Glowdisk <infernaldestinymc@gmail.com>
2025-02-27 20:43:25 -0800

    Fixing (attempting) vibrations

--- src/main/java/competition/subsystems/coral_scorer/commands/IntakeCoralCommand.java
+++ src/main/java/competition/subsystems/coral_scorer/commands/IntakeCoralCommand.java
@@ -1,23 +1,45 @@
 package competition.subsystems.coral_scorer.commands;
 
+import competition.operator_interface.OperatorInterface;
 import competition.subsystems.coral_scorer.CoralScorerSubsystem;
 import xbot.common.command.BaseCommand;
 import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.PropertyFactory;
 
 import javax.inject.Inject;
 
 public class IntakeCoralCommand extends BaseCommand {
-    CoralScorerSubsystem coral;
+    final CoralScorerSubsystem coral;
+    final OperatorInterface oi;
+    final DoubleProperty driverGamePadRumblePrefrence;
+    final DoubleProperty operatorGamePadRumblePrefrence;
 
     @Inject
-    public IntakeCoralCommand(CoralScorerSubsystem coralScorerSubsystem){
+    public IntakeCoralCommand(CoralScorerSubsystem coralScorerSubsystem, OperatorInterface oi, PropertyFactory pf){
         coral = coralScorerSubsystem;
+        this.oi = oi;
         this.addRequirements(coral);
+        pf.setPrefix(this);
+        driverGamePadRumblePrefrence = pf.createPersistentProperty("John/Driver Gamepad rumble level prefrence", .25);
+        operatorGamePadRumblePrefrence = pf.createPersistentProperty("Anthony/Operator Gamepad rumble level prefrence", .5);
     }
 
     @Override
     public void initialize() {
         coral.setCoralScorerState(CoralScorerSubsystem.CoralScorerState.INTAKING);
+    }
+
+
+    @Override
+    public void execute(){
+        System.out.println("Trigger is pressed");
+        if (coral.confidentlyHasCoral()) {
+            System.out.println("Execute happened - Rumble On");
+            oi.operatorGamepad.getRumbleManager().rumbleGamepad(operatorGamePadRumblePrefrence.get(), 1);
+            oi.driverGamepad.getRumbleManager().rumbleGamepad(driverGamePadRumblePrefrence.get(), 1);
+        } else {
+            System.out.println("IT DOESNT HAVE CORAL STILL");
+        }
     }
 
 }
