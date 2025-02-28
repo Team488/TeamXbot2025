30aca7363e5b2fb065e401fbc6b676c570ea57b3
anthonytrenh <trenhanthony@gmail.com>
2025-02-27 20:15:03 -0800

    flip target reef pose so that auto uses terminal approach on both alliances

--- src/main/java/competition/auto_programs/FromCageScoreOneCoralAutoFactory.java
+++ src/main/java/competition/auto_programs/FromCageScoreOneCoralAutoFactory.java
@@ -1,9 +1,11 @@
 package competition.auto_programs;
 
 import competition.commandgroups.DriveToFaceAndScoreCommandGroupFactory;
+import competition.simulation.MapleSimulator;
 import competition.subsystems.pose.Landmarks;
 import competition.subsystems.pose.PoseSubsystem;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import xbot.common.subsystems.autonomous.AutonomousCommandSelector;
 
@@ -12,14 +14,17 @@ import javax.inject.Inject;
 public class FromCageScoreOneCoralAutoFactory {
 
     final AutonomousCommandSelector autoSelector;
+    MapleSimulator mapleSimulator;
     PoseSubsystem pose;
     DriveToFaceAndScoreCommandGroupFactory driveToFaceAndScoreCommandGroupFact;
 
     @Inject
     public FromCageScoreOneCoralAutoFactory(AutonomousCommandSelector autoSelector,
-                                            DriveToFaceAndScoreCommandGroupFactory driveToFaceAndScoreCommandGroupFact,
-                                            PoseSubsystem pose){
+                                            MapleSimulator mapleSimulator,
+                                            PoseSubsystem pose,
+                                            DriveToFaceAndScoreCommandGroupFactory driveToFaceAndScoreCommandGroupFact){
         this.autoSelector = autoSelector;
+        this.mapleSimulator = mapleSimulator;
         this.pose = pose;
         this.driveToFaceAndScoreCommandGroupFact = driveToFaceAndScoreCommandGroupFact;
     }
@@ -31,6 +36,9 @@ public class FromCageScoreOneCoralAutoFactory {
 
         var startInFrontOfCage = pose.createSetPositionCommand(PoseSubsystem.convertBlueToRedIfNeeded(startingLocation));
         auto.addCommands(startInFrontOfCage);
+
+        var resetMapleSim = new InstantCommand(() -> mapleSimulator.resetPosition(PoseSubsystem.convertBlueToRedIfNeeded(startingLocation)));
+        auto.addCommands(resetMapleSim);
 
         auto.queueDriveAndScoreMessageToAutoSelector(targetReefFace, targetBranch, targetLevel);
         var driveAndScore = driveToFaceAndScoreCommandGroupFact.create(targetReefFace, targetBranch, targetLevel);
