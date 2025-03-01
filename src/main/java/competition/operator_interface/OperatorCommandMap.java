f18069fd5bbba27883a6bb940f952e6766ed4325
Ademfcan <ademfcan@gmail.com>
2025-03-01 00:25:22 -0800

    reef command over xtables

--- src/main/java/competition/operator_interface/OperatorCommandMap.java
+++ src/main/java/competition/operator_interface/OperatorCommandMap.java
@@ -5,6 +5,7 @@ import competition.auto_programs.FromCurrentPositionScoreFarLeftBranchALevelFour
 import competition.auto_programs.FromLeftCageScoreLeftFacesLevelFours;
 import competition.commandgroups.HeadingAssistedDriveAndScoreCommandGroup;
 import competition.commandgroups.vision_path.PathDriveToCoralStationAndIntakeUntilCollected;
+import competition.commandgroups.vision_path.PathToFaceAndScoreCommandGroupFactory;
 import competition.commandgroups.vision_path.PathToReefFaceThenAlignCommandGroupFactory;
 import competition.commandgroups.PrepAlgaeSystemCommandGroupFactory;
 import competition.commandgroups.PrepCoralSystemCommandGroupFactory;
@@ -29,6 +30,7 @@ import competition.subsystems.drive.commands.DriveToLocationWithPID;
 import competition.subsystems.drive.commands.RotateToHeadingWithHeadingModule;
 import competition.subsystems.drive.commands.SwerveDriveWithJoysticksCommand;
 import competition.subsystems.drive.commands.TeleportToPositionCommand;
+import competition.subsystems.drive.commands.vision_path.DriveToNearestOpenReef;
 import competition.subsystems.drive.commands.vision_path.PathDriveToNearestCoralStationSectionCommand;
 import competition.subsystems.drive.commands.vision_path.SwerveBezierTrajectoryCommand;
 import competition.subsystems.elevator.ElevatorSubsystem;
@@ -39,6 +41,7 @@ import competition.subsystems.oracle.commands.SuperstructureAccordingToOracleCom
 import competition.subsystems.pose.Cameras;
 import competition.subsystems.pose.Landmarks;
 import competition.subsystems.pose.commands.ResetPoseCommand;
+import competition.subsystems.vision.CoprocessorCommunicationSubsystem;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -93,7 +96,9 @@ public class OperatorCommandMap {
             SwerveBezierTrajectoryCommand sbtc,
             PathToReefFaceThenAlignCommandGroupFactory pathToReefFaceThenAlignCommandGroupFactory,
             PathDriveToCoralStationAndIntakeUntilCollected pathDriveToCoralStationAndIntakeUntilCollected,
-            HeadingAssistedDriveAndScoreCommandGroup.Factory headingAssistedDriveAndScoreCommandGroupFactory) {
+            HeadingAssistedDriveAndScoreCommandGroup.Factory headingAssistedDriveAndScoreCommandGroupFactory,
+            CoprocessorCommunicationSubsystem coprocessorCommunicationSubsystem,
+            PathToFaceAndScoreCommandGroupFactory pathToFaceAndScoreCommandGroupFactory) {
         resetHeading.setHeadingToApply(0);
         operatorInterface.driverGamepad.getifAvailable(XXboxController.XboxButton.Start).onTrue(resetHeading);
 
@@ -108,7 +113,7 @@ public class OperatorCommandMap {
         var oracleControlsRobot = Commands.parallel(driveAccordingToOracle, superstructureAccordingToOracle);
 
         var homed = prepCoralSystemCommandGroupFactory.create(() -> Landmarks.CoralLevel.COLLECTING);
-        operatorInterface.driverGamepad.getifAvailable(XXboxController.XboxButton.Y).whileTrue(pathDriveToCoralStationAndIntakeUntilCollected.create());
+        operatorInterface.driverGamepad.getifAvailable(XXboxController.XboxButton.Y).whileTrue(new DriveToNearestOpenReef(coprocessorCommunicationSubsystem,pathToFaceAndScoreCommandGroupFactory));
         var branchAHeadingAssistedDriveAndScore = headingAssistedDriveAndScoreCommandGroupFactory.create(Landmarks.Branch.A);
         operatorInterface.driverGamepad.getifAvailable(XXboxController.XboxButton.A).onTrue(branchAHeadingAssistedDriveAndScore)
                         .onFalse(homed);
---
+++ src/main/java/competition/subsystems/drive/commands/vision_path/DriveToNearestOpenReef.java
@@ -0,0 +1,66 @@
+package competition.subsystems.drive.commands.vision_path;
+import javax.print.attribute.standard.RequestingUserName;
+
+import competition.subsystems.pose.PoseSubsystem;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import org.kobe.xbot.JClient.XTablesClient;
+
+import competition.commandgroups.vision_path.PathToFaceAndScoreCommandGroupFactory;
+import competition.subsystems.pose.Landmarks;
+import competition.subsystems.vision.CoprocessorCommunicationSubsystem;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.DeferredCommand;
+
+import java.util.Set;
+
+public class DriveToNearestOpenReef extends DeferredCommand{
+
+    public DriveToNearestOpenReef(CoprocessorCommunicationSubsystem communicator, PathToFaceAndScoreCommandGroupFactory commandGroupFactory) {
+        super(() -> {
+            XTablesClient client = communicator.getXTablesManager().getOrNull();
+            if (client == null) {
+                return new InstantCommand(() -> System.out.println("Client is null, cannot generate command."));
+            }
+
+            try {
+                int bestAT = client.getInteger("BESTOPENREEF_AT");
+                int bestBranchIDX = client.getInteger("BESTOPENREEFBRANCH");
+
+                if (bestAT == -1 || bestBranchIDX == -1) {
+                    return new InstantCommand(() -> System.out.println("No valid reef found, skipping command."));
+                }
+
+                // Get the reef face, branch, and height
+                Landmarks.ReefFace face = Landmarks.getReefFaceFromTagId(bestAT);
+                Landmarks.Branch branch = (bestBranchIDX % 2 == 0) ? Landmarks.Branch.A : Landmarks.Branch.B;
+                Landmarks.CoralLevel height = getLevel(bestBranchIDX);
+
+                // Return the created command group
+                return commandGroupFactory.create(face, branch, height);
+
+            } catch (IllegalArgumentException illegalArgs) {
+                System.out.println("Exception occurred: " + illegalArgs.toString());
+                return new InstantCommand(() -> System.out.println("Exception in DriveToNearestOpenReef"));
+            }
+        }, Set.of());
+    }
+
+    private static Landmarks.CoralLevel getLevel(int branchIdx) {
+        int height = (branchIdx / 2) + 1;  // Calculate height
+
+        switch (height) {  // Use height instead of getLevel
+            case 1:
+                return Landmarks.CoralLevel.ONE;
+            case 2:
+                return Landmarks.CoralLevel.TWO;
+            case 3:
+                return Landmarks.CoralLevel.THREE;
+            case 4:
+                return Landmarks.CoralLevel.FOUR;
+            default:
+                throw new IllegalArgumentException("Invalid branchIdx: " + branchIdx);
+        }
+    }
+}
