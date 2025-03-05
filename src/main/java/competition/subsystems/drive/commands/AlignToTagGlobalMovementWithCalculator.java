f056a8545a0457711b0f634f913615b72fc2fc2a
JohnGilb <johngilb@gmail.com>
2025-03-04 18:26:57 -0800

    Vision suppression still collects vision updates - it just shifts the primary reporter of pose to the wheels/gyro odometry while vision is suppressed.

--- src/main/java/competition/subsystems/drive/commands/AlignToTagGlobalMovementWithCalculator.java
+++ src/main/java/competition/subsystems/drive/commands/AlignToTagGlobalMovementWithCalculator.java
@@ -5,10 +5,8 @@ import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.pose.PoseSubsystem;
 import competition.subsystems.drive.logic.AlignCameraToAprilTagCalculator;
 import competition.subsystems.vision.AprilTagVisionSubsystemExtended;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.Distance;
 import xbot.common.command.BaseCommand;
-import xbot.common.math.XYPair;
 import xbot.common.subsystems.drive.control_logic.HeadingModule;
 
 import javax.inject.Inject;
@@ -77,7 +75,7 @@ public class AlignToTagGlobalMovementWithCalculator extends BaseCommand {
 
         calculator.configureAndReset(targetAprilTagID, targetCameraID, offset,
                 isCameraBackwards, startingActivity, requireExcellentAlignment);
-        pose.setAreVisionUpdatesDisabled(true);
+        pose.setDisableVisionUpdatesAndPrioritizeOdometry(true);
     }
 
     @Override
@@ -98,7 +96,7 @@ public class AlignToTagGlobalMovementWithCalculator extends BaseCommand {
 
     @Override
     public void end(boolean interrupted) {
-        pose.setAreVisionUpdatesDisabled(false);
+        pose.setDisableVisionUpdatesAndPrioritizeOdometry(false);
         drive.stop();
     }
 }
