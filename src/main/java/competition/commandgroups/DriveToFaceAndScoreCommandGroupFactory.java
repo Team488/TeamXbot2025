beb11b91982d4d2aae4c0c0d1b54a217fded3918
anthonytrenh <trenhanthony@gmail.com>
2025-03-04 18:38:55 -0800

    Update DriveToFaceAndScoreCommandGroupFactory.java

--- src/main/java/competition/commandgroups/DriveToFaceAndScoreCommandGroupFactory.java
+++ src/main/java/competition/commandgroups/DriveToFaceAndScoreCommandGroupFactory.java
@@ -35,11 +35,11 @@ public class DriveToFaceAndScoreCommandGroupFactory {
         this.prepCoralSystemFactory = prepCoralSystemFactory;
         this.scoreWhenReadyProvider = scoreWhenReadyProvider;
 
+        pf.setPrefix("DriveToFaceAndScoreCommandGroupFactory");
         levelOneDistanceThreshold = pf.createPersistentProperty("LevelOneDistanceThresholdInMeters", 0.5);
         levelTwoDistanceThreshold = pf.createPersistentProperty("LevelTwoDistanceThresholdInMeters", 0.5);
         levelFourDistanceThreshold = pf.createPersistentProperty("LevelFourDistanceThresholdInMeters", 0.5);
 
-        pf.setPrefix("DriveToFaceAndScoreCommandGroupFactory");
     }
 
     public SequentialCommandGroup create(Landmarks.ReefFace targetReefFace,
