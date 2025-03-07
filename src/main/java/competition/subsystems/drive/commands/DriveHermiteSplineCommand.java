976c39ec4d7c1d90f7ea57506996fa4849ddee52
John Gilbert <johngilb@gmail.com>
2025-03-07 13:14:42 -0800

    Allow for path recovery - also temporarily add joystick to induce bad paths.

--- src/main/java/competition/subsystems/drive/commands/DriveHermiteSplineCommand.java
+++ src/main/java/competition/subsystems/drive/commands/DriveHermiteSplineCommand.java
@@ -4,6 +4,8 @@ import competition.motion.CubicHermiteSpline;
 import competition.motion.CubicHermiteSplineParameters;
 import competition.motion.HermiteTrajectory;
 import competition.motion.HermiteTrajectoryAdvice;
+import competition.operator_interface.OperatorCommandMap;
+import competition.operator_interface.OperatorInterface;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.pose.PoseSubsystem;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -18,15 +20,17 @@ public class DriveHermiteSplineCommand extends BaseCommand {
 
     final DriveSubsystem drive;
     final PoseSubsystem pose;
+    final OperatorInterface oi;
 
 
     HermiteTrajectory trajectory;
     final Translation2d startPoint;
 
     @Inject
-    public DriveHermiteSplineCommand(DriveSubsystem drive, PoseSubsystem pose) {
+    public DriveHermiteSplineCommand(DriveSubsystem drive, PoseSubsystem pose, OperatorInterface oi) {
         this.drive = drive;
         this.pose = pose;
+        this.oi = oi;
 
         trajectory = new HermiteTrajectory();
         CubicHermiteSpline spline = new CubicHermiteSpline();
@@ -61,17 +65,30 @@ public class DriveHermiteSplineCommand extends BaseCommand {
         XYPair velocityIntent = new XYPair(
                 advice.velocity().getX() / drive.getMaxTargetSpeedMetersPerSecond(),
                 advice.velocity().getY() / drive.getMaxTargetSpeedMetersPerSecond());
-        /*if (advice.timeFrozen()) {
-            velocityIntent = new XYPair(0, 0);
-        }
+        if (advice.timeFrozen()) {
+            // If the timer is frozen, that means we have gone off-track somewhere and need to catch back up. We have to
+            // be careful here - if we just set the velocity component to 0, we will use PID to approach, but as soon as we
+            // do the ghost will shoot ahead as it expects us to have much higher velocities. Instead, we need to use
+            // the portion of the velocity that is in the direction of the ghost. We can find the similarity of the vector
+            // using the dot product, and use that dot product to scale the velocity vector.
 
-         */
+            var directionToGhost = advice.position().minus(currentPosition);
+            var directionToGhostUnitVector = XYPair.fromUnitPolar(directionToGhost.getAngle().getDegrees());
+            var velocityIntentUnitVector = XYPair.fromUnitPolar(velocityIntent.getAngle());
+
+            var similarity = directionToGhostUnitVector.dotProduct(velocityIntentUnitVector);
+            velocityIntent = velocityIntent.clone().scale(similarity);
+        }
 
         // Position - spline will be outputting a position that we should be at. We need to PID to that position.
         var positionTranslation = drive.getPowerToAchieveFieldPosition(currentPosition, advice.position());
         XYPair positionIntent = new XYPair(positionTranslation.getX(), positionTranslation.getY());
 
         var fusedIntent = velocityIntent.add(positionIntent);
+
+        if (oi.driverGamepad.getLeftVector().getNorm() > 0.2) {
+            fusedIntent = new XYPair(oi.driverGamepad.getLeftVector().getX(), oi.driverGamepad.getLeftVector().getY());
+        }
 
         drive.fieldOrientedDrive(fusedIntent, 0, pose.getCurrentHeading().getDegrees(), true);
 
