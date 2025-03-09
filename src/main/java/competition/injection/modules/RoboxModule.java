bae12db4da772bead5bdb38f99fce6df78c7306e
JohnGilb <johngilb@gmail.com>
2025-03-08 21:00:34 -0800

    Feature/sammamish day 1 (#272)
    * Support the Pigeon 2 IMU on the 2025 robot
    
    This also tracks accelerations over time, we might be able to use this for collision detection in future.
    
    * Updates
    
    * Newest SCL
    
    * keep mock
    
    * Change default wait time after scoring to add some stability
    
    * Wait for a full second after chassis is aligned before attempting to score in auto. This will allow the superstructure some time to settle.
    
    * Remove safeties from algae arm
    
    * More safeties in approach
    
    * Change calibration
    
    * better waits
    
    * better wait defaults
    
    ---------
    
    Co-authored-by: Stephen Just <stephenjust@gmail.com>

--- src/main/java/competition/injection/modules/RoboxModule.java
+++ src/main/java/competition/injection/modules/RoboxModule.java
@@ -4,17 +4,8 @@ import competition.electrical_contract.ElectricalContract;
 import competition.electrical_contract.RoboxContract;
 import competition.simulation.BaseSimulator;
 import competition.simulation.NoopSimulator;
-import competition.subsystems.drive.DriveSubsystem;
-import competition.subsystems.pose.PoseSubsystem;
-import competition.subsystems.vision.AprilTagVisionSubsystemExtended;
 import dagger.Binds;
 import dagger.Module;
-import xbot.common.injection.electrical_contract.XCameraElectricalContract;
-import xbot.common.injection.electrical_contract.XSwerveDriveElectricalContract;
-import xbot.common.subsystems.drive.BaseDriveSubsystem;
-import xbot.common.subsystems.drive.BaseSwerveDriveSubsystem;
-import xbot.common.subsystems.pose.BasePoseSubsystem;
-import xbot.common.subsystems.vision.AprilTagVisionSubsystem;
 
 import javax.inject.Singleton;
 
--- src/main/java/competition/injection/modules/UnitTestRobotModule.java
+++ src/main/java/competition/injection/modules/UnitTestRobotModule.java
@@ -1,20 +1,9 @@
 package competition.injection.modules;
 
-import competition.electrical_contract.ElectricalContract;
-import competition.electrical_contract.UnitTestContract2025;
 import competition.simulation.BaseSimulator;
 import competition.simulation.MapleSimulator;
-import competition.subsystems.drive.DriveSubsystem;
-import competition.subsystems.pose.PoseSubsystem;
-import competition.subsystems.vision.AprilTagVisionSubsystemExtended;
 import dagger.Binds;
 import dagger.Module;
-import xbot.common.injection.electrical_contract.XCameraElectricalContract;
-import xbot.common.injection.electrical_contract.XSwerveDriveElectricalContract;
-import xbot.common.subsystems.drive.BaseDriveSubsystem;
-import xbot.common.subsystems.drive.BaseSwerveDriveSubsystem;
-import xbot.common.subsystems.pose.BasePoseSubsystem;
-import xbot.common.subsystems.vision.AprilTagVisionSubsystem;
 
 import javax.inject.Singleton;
 
