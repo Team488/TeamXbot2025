bee38c81cbfb09d8ce3bab4c9036f1d8c328162f
Stephen Just <stephenjust@gmail.com>
2025-03-13 23:22:38 -0700

    Transform SCL and WPILib Command Framework to get more instrucmentation

--- src/main/java/competition/aspects/CommandTracer.java
+++ src/main/java/competition/aspects/CommandTracer.java
@@ -2,6 +2,7 @@ package competition.aspects;
 
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -52,14 +53,14 @@ public class CommandTracer {
         }
     }
 
-    @Before("execution(* edu.wpi.first.wpilibj2.command.Command.initialize(..))" +
-            "|| execution(* edu.wpi.first.wpilibj2.command.Command.execute(..))" +
-            "|| execution(* edu.wpi.first.wpilibj2.command.Command.end(..))")
+    @Before("execution(* edu.wpi.first.wpilibj2.command.Command+.initialize(..))" +
+            "|| execution(* edu.wpi.first.wpilibj2.command.Command+.execute(..))" +
+            "|| execution(* edu.wpi.first.wpilibj2.command.Command+.end(..))")
     public void logCommand(JoinPoint joinPoint) {
         var enableTracing = Preferences.getBoolean(PREFERENCES_KEY, false);
         if (openedFile && enableTracing) {
             try {
-                writer.write(String.format("%f: -> %s\n", Timer.getTimestamp(), joinPoint.toShortString()));
+                writer.write(String.format("%f: -> %s %s\n", Timer.getTimestamp(), ((Command)joinPoint.getThis()).getName(), joinPoint.toShortString()));
             } catch (IOException ignored) {}
         }
     }
