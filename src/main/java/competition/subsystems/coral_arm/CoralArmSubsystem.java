02f14e2be3f446bb9d068678c5d150e4c55b9b80
Glowdisk <infernaldestinymc@gmail.com>
2025-03-15 12:50:55 -0700

    Super small number change

--- src/main/java/competition/subsystems/coral_arm/CoralArmSubsystem.java
+++ src/main/java/competition/subsystems/coral_arm/CoralArmSubsystem.java
@@ -92,11 +92,11 @@ public class CoralArmSubsystem extends BaseSetpointSubsystem<Angle> {
         }
 
         this.level123ScoringAngle = propertyFactory.createPersistentProperty("Level 1/2/3 Scoring Angle", 137);
-        this.level4ScoringAngle = propertyFactory.createPersistentProperty("Level 4 Scoring Angle", 172);
+        this.level4ScoringAngle = propertyFactory.createPersistentProperty("Level 4 Scoring Angle", 162);
         propertyFactory.setDefaultLevel(PropertyLevel.Debug);
         this.humanLoadAngleDegrees = propertyFactory.createPersistentProperty("Human Loading Angle in Degrees", 0);
         this.degreesPerRotations = propertyFactory.createPersistentProperty("Degrees Per Rotations", 5.790);
-        this.rangeOfMotionDegrees = propertyFactory.createPersistentProperty("Range of Motion in Degrees", 172);
+        this.rangeOfMotionDegrees = propertyFactory.createPersistentProperty("Range of Motion in Degrees", 162);
         this.powerWhenNotCalibrated = propertyFactory.createPersistentProperty("Power When Not Calibrated", 0.25);
         propertyFactory.setDefaultLevel(PropertyLevel.Important);
     }
