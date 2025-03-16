08e63c4de17830ef994afd268ee1cccec1faca69
John Kuang <140275276+kujo27@users.noreply.github.com>
2025-03-16 13:38:12 -0700

    Tuning L4 Sequence and AlignToAprilTag (#302)
    * speeding things up
    
    * deleted testing command
    
    * change persistent properties

--- src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/competition/subsystems/elevator/ElevatorSubsystem.java
@@ -94,8 +94,8 @@ public class ElevatorSubsystem extends BaseSetpointSubsystem<Distance> {
 
         pf.setPrefix(this);
 
-        l2Height = pf.createPersistentProperty("l2Height", Inches.of(0.5));
-        l3Height = pf.createPersistentProperty("l3Height", Inches.of(16.0));
+        l2Height = pf.createPersistentProperty("l2Height", Inches.of(1.0));
+        l3Height = pf.createPersistentProperty("l3Height", Inches.of(17.0));
         l4Height = pf.createPersistentProperty("l4Height", Inches.of(46.0));
         humanLoadHeight = pf.createPersistentProperty("humanLoadHeight", Inches.of(1));
         pf.setDefaultLevel(PropertyLevel.Debug);
