fa41e11951a308ee8b1e2b7f115b363424d48ecc
JohnGilb <johngilb@gmail.com>
2025-03-22 15:33:05 -0700

    Make human loading an important property

--- src/main/java/competition/subsystems/coral_arm/CoralArmSubsystem.java
+++ src/main/java/competition/subsystems/coral_arm/CoralArmSubsystem.java
@@ -91,10 +91,11 @@ public class CoralArmSubsystem extends BaseSetpointSubsystem<Angle> {
             this.lowSensor = null;
         }
 
+        propertyFactory.setDefaultLevel(PropertyLevel.Important);
         this.level123ScoringAngle = propertyFactory.createPersistentProperty("Level 1 2 3 Scoring Angle", 136);
         this.level4ScoringAngle = propertyFactory.createPersistentProperty("Level 4 Scoring Angle", 166);
-        propertyFactory.setDefaultLevel(PropertyLevel.Debug);
         this.humanLoadAngleDegrees = propertyFactory.createPersistentProperty("Human Loading Angle in Degrees", 0);
+        propertyFactory.setDefaultLevel(PropertyLevel.Debug);
         this.degreesPerRotations = propertyFactory.createPersistentProperty("Degrees Per Rotations", 5.790);
         this.rangeOfMotionDegrees = propertyFactory.createPersistentProperty("Range of Motion in Degrees", 170);
         this.powerWhenNotCalibrated = propertyFactory.createPersistentProperty("Power When Not Calibrated", 0.25);
