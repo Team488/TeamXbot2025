a840a5a28fa07a3c84436a31e938dcd1e5fdec3f
Stephen Just <stephenjust@gmail.com>
2025-03-29 10:57:00 -0700

    Review comments

--- src/main/java/competition/aspects/DriverStationOverrides.java
+++ src/main/java/competition/aspects/DriverStationOverrides.java
@@ -1,6 +1,7 @@
 package competition.aspects;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Preferences;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -9,7 +10,13 @@ import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class DriverStationOverrides {
-    public static final Logger log = LogManager.getLogger(DriverStation.class);
+    private static final Logger log = LogManager.getLogger(DriverStation.class);
+
+    private static final String DISABLE_LOOP_OVERRUN_PRINTING_KEY = "DriverStation/disableLoopOverrunPrinting";
+
+    public DriverStationOverrides() {
+        Preferences.initBoolean(DISABLE_LOOP_OVERRUN_PRINTING_KEY, true);
+    }
 
     @Around("execution(* edu.wpi.first.wpilibj.DriverStation.reportWarning(java.lang.String, boolean))")
     public void reportWarningOverride(ProceedingJoinPoint joinPoint) {
@@ -17,7 +24,9 @@ public class DriverStationOverrides {
     }
 
     @Around("execution(* edu.wpi.first.wpilibj.Watchdog.printEpochs(..))")
-    public void disableLoopOverrunPrinting(ProceedingJoinPoint joinPoint) {
-        // Do nothing
+    public void disableLoopOverrunPrinting(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (!Preferences.getBoolean(DISABLE_LOOP_OVERRUN_PRINTING_KEY, true)) {
+            joinPoint.proceed();
+        }
     }
 }
