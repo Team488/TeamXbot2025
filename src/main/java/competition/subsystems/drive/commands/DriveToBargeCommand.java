5ca48ac225ec9ae672236c4fc8f8135f0622ebc7
Agent Smith <488matrix@gmail.com>
2025-03-29 20:11:33 -0700

    awd

--- src/main/java/competition/subsystems/drive/commands/DriveToBargeCommand.java
+++ src/main/java/competition/subsystems/drive/commands/DriveToBargeCommand.java
@@ -6,6 +6,7 @@ import competition.subsystems.vision.CoprocessorCommunicationSubsystem;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
+import org.kobe.xbot.Utilities.Entities.XTableValues;
 import xbot.common.logging.RobotAssertionManager;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
@@ -26,6 +27,7 @@ public class DriveToBargeCommand extends PathDriveToLocationCommand {
                                RobotAssertionManager robotAssertionManager,
                                CoprocessorCommunicationSubsystem coprocessorCommunicationSubsystem) {
         super(drive, pose, pf, headingModuleFactory, robotAssertionManager, coprocessorCommunicationSubsystem);
+        pf.setPrefix("DriveToBargeCommand");
         distanceFromBarge = pf.createPersistentProperty("distanceFromBarge-m", 1);
         midFieldX = pf.createPersistentProperty("midFieldX-m", 8.75665);
     }
@@ -36,8 +38,10 @@ public class DriveToBargeCommand extends PathDriveToLocationCommand {
         Pose2d goal = new Pose2d(
                 blue ? midFieldX.get() - distanceFromBarge.get() : midFieldX.get() + distanceFromBarge.get(),
                 pose.getCurrentPose2d().getY(),
-                blue ? Rotation2d.fromDegrees(180) : Rotation2d.fromDegrees(0)
+                Rotation2d.fromDegrees(0)
         );
+        super.setOptions(XTableValues.TraversalOptions.newBuilder()
+                .setFinalRotationDegrees(blue ? 180 : 0).build());
         super.setTarget(goal);
         super.initialize();
     }
