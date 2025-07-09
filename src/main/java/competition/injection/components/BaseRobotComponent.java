cc3b3e0852690723f4a14d8523565f285c7723d9
Alex Schokking <aschokking@gmail.com>
2025-07-09 12:44:49 -0700

    Giant delete of tons of commands

--- src/main/java/competition/injection/components/BaseRobotComponent.java
+++ src/main/java/competition/injection/components/BaseRobotComponent.java
@@ -2,7 +2,6 @@ package competition.injection.components;
 
 import competition.electrical_contract.ElectricalContract;
 import competition.motion.TrapezoidProfileManager;
-import competition.operator_interface.NeoTrellisSubsystem;
 import competition.operator_interface.OperatorCommandMap;
 import competition.operator_interface.OperatorInterface;
 import competition.simulation.BaseSimulator;
@@ -58,8 +57,6 @@ public abstract class BaseRobotComponent extends BaseComponent {
     public abstract LightSubsystem lightSubsystem();
 
     public abstract TrapezoidProfileManager.Factory trapezoidProfileManagerFactory();
-
-    public abstract NeoTrellisSubsystem neoTrellisSubsystem();
 
     public abstract OperatorInterface operatorInterface();
 
